6be91f9d7633378b09fcda212499911f98a51fc2
shakeel0232 <66824177+shakeel0232@users.noreply.github.com>
2021-07-12 13:45:59 +0500

    Update TestPackages.dart

--- lib/TestPackages.dart
+++ lib/TestPackages.dart
@@ -5,3 +5,8 @@ class Calculator {
   /// Returns [value] plus 1.
   int addOne(int value) => value + 1;
 }
+
+class Calculator2 {
+  /// Returns [value] plus 1.
+  int addOne(int value) => value + 1;
+}
