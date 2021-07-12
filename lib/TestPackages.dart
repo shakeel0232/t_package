6e71f071905e386bc9dfd493f5259dca5700983d
shakeel0232 <66824177+shakeel0232@users.noreply.github.com>
2021-07-12 13:54:37 +0500

    s

--- lib/TestPackages.dart
+++ lib/TestPackages.dart
@@ -1,5 +1,5 @@
 library TestPackages;
-
+part 'class5.dart';
 /// A Calculator.
 class Calculator {
   /// Returns [value] plus 1.
@@ -10,10 +10,13 @@ class Calculator2 {
   /// Returns [value] plus 1.
   int addOne(int value) => value + 1;
 }
+
 class Calculator3 {
   /// Returns [value] plus 1.
   int addOne(int value) => value + 1;
-}class Calculator4 {
+}
+
+class Calculator4 {
   /// Returns [value] plus 1.
   int addOne(int value) => value + 1;
 }
---
+++ lib/class5.dart
@@ -0,0 +1,6 @@
+part of TestPackages;
+
+class Calculator5 {
+  /// Returns [value] plus 1.
+  int addOne(int value) => value + 1;
+}
